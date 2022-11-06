00c120b9da9ee9871766afa9a1bc899f031200e6
jay-rt <thapa.jayrt@gmail.com>
2022-11-05 22:28:04 -0400

    Added product items and added hover animation

--- src/data.js
+++ src/data.js
@@ -39,3 +39,38 @@ export const sliderItems = [
       title: "LIGHT JACKETS",
     },
   ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+      id:2,
+      img:"https://pngimg.com/uploads/tshirt/tshirt_PNG5446.png",
+    },
+    {
+      id:3,
+      img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+      id:4,
+      img:"https://www.miumiu.com/content/dam/miumiu_products/M/MG1/MG1670/1ZP6F0BDK/MG1670_1ZP6_F0BDK_SLF.png",
+    },
+    {
+      id:5,
+      img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+      id:6,
+      img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+      id:7,
+      img:"https://stellar.centracdn.net/client/dynamic/images/496_fc35f30568-219_yellow_01_stellar_down_parka.png",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
