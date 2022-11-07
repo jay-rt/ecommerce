09ccff9bd7f59eee166e96019fa12986a39de7df
jay-rt <thapa.jayrt@gmail.com>
2022-11-06 21:51:46 -0500

    Added a single product page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Newsletter from "./components/Newsletter";
 import Home from "./pages/Home";
+import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/productlist" element={<ProductList />}></Route>
+          <Route path="/product" element={<Product />}></Route>
         </Routes>
         <Newsletter />
         <Footer />
