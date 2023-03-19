7d53f0f7bf0c34f353c10bc6e6c730a42e0220b6
jay-rt <thapa.jayrt@gmail.com>
2023-03-19 00:26:12 -0400

    changed the link for the server

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 4000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://ecommerce-api-l9w0.onrender.com",
         changeOrigin: true,
         secure: false,
       },
